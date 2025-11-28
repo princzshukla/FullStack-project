84564171e2c33f15f367f9ef61e77d986fc584a2
prince shukla <princeshukla3264@gmail.com>
2025-11-28 20:02:58 +0530

    feat: Add server proxy configuration for API requests

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,11 @@ import react from "@vitejs/plugin-react";
 
 // Remove any tailwindcss import line
 export default defineConfig({
+  server: {
+    proxy:{
+      '/api': 'http://localhost:6000'
+
+    }
+  },
   plugins: [react()],
 });
